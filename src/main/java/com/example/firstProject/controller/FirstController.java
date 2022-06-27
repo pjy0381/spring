aedb72f0c8561c4392c3729ec4f4c2335c27b832
pjy0381 <jo101044@naver.com>
2022-06-27 13:04:35 +0900

    api예제

--- src/main/java/com/example/firstProject/controller/FirstController.java
+++ src/main/java/com/example/firstProject/controller/FirstController.java
@@ -3,8 +3,9 @@ package com.example.firstProject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class FirstController {
 
     @GetMapping("/hi")
