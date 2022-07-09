7e34c14b49efcb89b0cc782ff770cc444bd53ef6
pjy0381 <jo101044@naver.com>
2022-07-09 23:15:24 +0900

    댓글 수정기능 추가

--- src/main/java/com/example/firstProject/api/CommentApiController.java
+++ src/main/java/com/example/firstProject/api/CommentApiController.java
@@ -17,13 +17,13 @@ public class CommentApiController {
     @Autowired
     private CommentService commentService;
 
-    @GetMapping("/api/articles/{articleId}/comments")
+    @GetMapping("/api/comments/{articleId}/comments")
     public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId){
         List<CommentDto> dtos = commentService.comments(articleId);
         return ResponseEntity.status(HttpStatus.OK).body(dtos);
     }
 
-    @PostMapping("/api/articles/{articleId}/comments")
+    @PostMapping("/api/comments/{articleId}/comments")
     public  ResponseEntity<CommentDto> create(@PathVariable Long articleId,
                                               @RequestBody CommentDto dto){
         CommentDto createdDto = commentService.create(articleId,dto);
@@ -31,7 +31,7 @@ public class CommentApiController {
         return ResponseEntity.status(HttpStatus.OK).body(createdDto);
     }
 
-    @PatchMapping("/api/articles/{id}/comments")
+    @PatchMapping("/api/comments/{id}/comments")
     public  ResponseEntity<CommentDto> update(@PathVariable Long id,
                                               @RequestBody CommentDto dto){
         CommentDto updatedDto = commentService.update(id,dto);
@@ -39,7 +39,7 @@ public class CommentApiController {
         return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
     }
 
-    @DeleteMapping("/api/articles/{id}/comments")
+    @DeleteMapping("/api/comments/{id}/comments")
     public  ResponseEntity<CommentDto> delete(@PathVariable Long id){
         CommentDto deletedDto = commentService.delete(id);
 
